be60c0f654cb8ad0273803ac11c733676b5cacbd
ljkLionel <42372664+lionlim97@users.noreply.github.com>
2019-10-15 21:22:33 +0800

    Revert "Sync fork"

--- src/main/java/Tasks/Event.java
+++ src/main/java/Tasks/Event.java
@@ -38,8 +38,4 @@ public class Event extends Task {
         return by + " " + start + " to " + end;
     }
 
-    @Override
-    public String toShow() {
-        return "Start: " + start + "\nEnd: " + end + "\n";// + super.toShow();
-    }
 }
